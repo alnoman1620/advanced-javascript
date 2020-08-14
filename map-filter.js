bd8fdd33483eaa890498b125d7e2d4a931236308
alnoman1620 <67537851+alnoman1620@users.noreply.github.com>
2020-08-14 20:29:28 +0600

    math,find,filter

---
+++ map-filter.js
@@ -0,0 +1,16 @@
+// const num = [2, 3, 4, 5];
+// const result = num.map(function (element) {
+//   return element * element;
+// });
+// console.log(result);
+
+// filter
+// const num = [2, 3, 4, 5];
+// const bigger = num.filter((x) => x < 5);
+
+// console.log(bigger);
+
+// find//
+const num = [2, 3, 4, 5, 6, 7];
+const isThere = num.find((x) => x > 5);
+console.log(isThere);
