584918ef3c983f82d156013ae63822673c183d73
alnoman1620 <67537851+alnoman1620@users.noreply.github.com>
2020-08-14 22:12:17 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,14 @@
+let bonus = 20;
+
+function sum(first, second) {
+  let result = first + second + bonus;
+  //   console.log(bonus);
+  if (result > 9) {
+    const mood = "good";
+    console.log(mood);
+  }
+  return result;
+}
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
