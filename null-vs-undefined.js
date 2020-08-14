0862178c5da5257bc95a2ff970c21ebfd8d32fbd
alnoman1620 <67537851+alnoman1620@users.noreply.github.com>
2020-08-14 18:50:12 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+// // let robin;
+// function add(num1, num2) {
+//   console.log(num1 + num2);
+//   return;
+// }
+
+// function add(num1, num2) {
+//   console.log(num1, num2);
+// }
+
+// const result = add(13);
+// console.log(result);
+
+// const student = { name: "sourav", id: 120, phone: 01645635465 };
+// console.log(student.uni);
+
+let fun = undefined;
